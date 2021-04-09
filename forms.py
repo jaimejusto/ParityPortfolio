19fa38de5b060f93406323264d19e1d66ac10eab
Jaime <justoj@oregonstate.edu>
2021-04-08 19:00:39 -0700

    added login

--- forms.py
+++ forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import Length, DataRequired, Email
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, InputRequired, Length, EqualTo, Email
 
-class RegistrationForm(FlaskForm):
+
+class LoginForm(FlaskForm):
     # username must be between 5 and 15 characters
-    username =StringField('Username', validators=[DataRequired(), Length(min=5, max=15)])
-    
+    username = StringField("Username", validators=[
+        InputRequired(), 
+        Length(min=5, max=15, message="Username must be between 5 and 15 characters long.")])
+    password = PasswordField("Password", validators=[InputRequired()]) 
+    submit = SubmitField("Login")
--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-from flask import Flask, render_template, url_for
-from forms import RegistrationForm
-app = Flask(__name__)
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf.csrf import CSRFProtect
+import forms
 
-preset_data = [
-    {
-        "one_thing": "info about one thing",
-        "two_thing": "info about two thing",
-        "three_thing": "info about three thing"
-    },
-    {
-        "one_thing": "info about another one thing blah blah blah",
-        "two_thing": "info about another two thing blah blah blah",
-        "three_thing": "info about three thing again blah blah"
-    }
-]
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+csrf = CSRFProtect(app)
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        return redirect('userDashboard')
+    return render_template('login.html', form = form)
+
+@app.route('/userDashboard')
+def userDashboard():
+    return render_template('userDashboard.html')
 @app.route("/presets")
 def json_dump():
     return render_template('presets.html', title='Base Data', preset_data = preset_data)
